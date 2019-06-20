4d3107c03a110f449810748a0f300c293fe4de24
iotronlab <50877415+iotronlab@users.noreply.github.com>
2019-06-20 18:49:43 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+
+module.exports = {
+    
+  entry: ['./src/style.scss', './src/index.js'],
+  output: {
+    filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+  },
+    
+    devtool: 'inline-source-map',
+    devServer: {
+    contentBase: './dist',
+    },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          {loader: 'extract-loader'},
+          {loader: 'css-loader'},
+          {loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['./node_modules'],
+            },
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
+      }
+    ],
+  },
+};
